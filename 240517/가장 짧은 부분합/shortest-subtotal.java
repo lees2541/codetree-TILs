ed4fdb6cd7f276f5b078d8c2ff3dd377ab10521c
lees2541 <81253958+lees2541@users.noreply.github.com>
2024-05-17 20:26:37 +0900

    Title: 가장 짧은 부분합, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240517/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//	 write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n+1];
+        int[] sum = new int[n+1];
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+        sum[0] = arr[0];
+        for (int i = 1;i<n;i++){
+            sum[i] = sum[i-1] + arr[i];
+        }
+        int mir = n;
+        int r;
+        for(int i = 0, j = 0;i < n ; i++){
+            if(sum[j]-sum[i]>= m){
+                r = j - i ;
+                if (r < mir) mir = r;
+                j = i+1;
+                continue;
+            }else{
+                if(n-j == 1){
+                    j = i+1;
+                    continue;
+                }else{
+                    i--;
+                    j++;
+                    continue;
+                }
+                
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.printf("%d" ,mir);
+
+    }
+}
