be0686d0dd8fac5dafb4ce50db24e9dbf03fea3a
lees2541 <81253958+lees2541@users.noreply.github.com>
2024-05-17 20:42:11 +0900

    Title: 피보나치 수, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240517/피보나치 수/fibonacci-number.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//	 write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.printf("%d", arr[n-1]);
+
+    }    
+}
